5ab5236acb41ea78095a4cfc23131aa96b602e66
Jordan Nolan <jordanwn@gmail.com>
2013-08-08 22:41:42 -0700

    updated index.html, routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 PersonalWebsite::Application.routes.draw do
-  get "personal_website/index"
+  # get "personal_website/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'personal_website#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
